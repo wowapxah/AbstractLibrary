55ae051a8479cdc2a8bedf94364c4fbe0e595bd9
wowapxah@gmail.com <3k@n@z_wtgm_y@_le6t_n0v!>
2020-04-06 22:16:46 +0300

    Создание проекта

---
+++ src/main/java/ru/wowapxah/AbstractLibrary/AbstractLibraryApplication.java
@@ -0,0 +1,13 @@
+package ru.wowapxah.AbstractLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbstractLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbstractLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/wowapxah/AbstractLibrary/AbstractLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.wowapxah.AbstractLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbstractLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
